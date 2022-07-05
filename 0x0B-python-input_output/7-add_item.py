440e4ed734510fcbc6fa27bd1d12345667492495
Ibegbulem Chukwuma Victor <ibechuksvictor@gmail.com>
2022-07-05 14:23:10 -0700

    task 7 - fix bug

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-'''Module for saving argv info via json to file.'''
-import json
-import os.path
+"""
+    module to add items to a list and save the list
+    as a json string
+"""
+
+import os
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-file = "add_item.json"
-json_list = []
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
 
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+file_is_present = os.path.exists("add_item.json")
+if not file_is_present:
+    save_to_json_file([], "add_item.json")
+
+if len(sys.argv) < 2:
+    exit(0)
+
+my_list = load_from_json_file("add_item.json")
 for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
-
-save_to_json_file(json_list, file)
+    my_list.append(sys.argv[i])
+save_to_json_file(my_list, "add_item.json")
